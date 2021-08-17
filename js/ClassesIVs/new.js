5d6bef866affdf6ccb845fe1e036830070dc9189
asasin235 <aatif18789@st.jmi.ac.in>
2021-08-17 12:02:46 +0530

    Added save to file

---
+++ js/ClassesIVs/new.js
@@ -0,0 +1,9 @@
+
+// let firstNum=prompt("Enter first Number")
+// let secondNum = prompt("Enter secondNumber")
+// firstNum=parseInt(firstNum)
+// secondNum=parseInt(secondNum)
+// let sum=0
+// sum=firstNum+secondNum
+//alert(sum)
+// console.log(sum)
